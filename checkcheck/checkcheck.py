3dffcb8a6e087315b8c0c880624801243fa28380
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2016-09-06 22:39:19 -0700

    initial commit

---
+++ checkcheck/checkcheck.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+"""CheckCheck checks repositories where the file name is the checksum"""
+import argparse
+import hashlib
+import os
+import logging
+import sys
+
+
+class CheckCheckException(Exception):
+    """Raise for checksum errors"""
+
+logger = logging.getLogger(__name__)
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description='fixity check for content addressable storage')
+
+    parser.add_argument('store', help='root path or specific file')
+
+    parser.add_argument('--loglevel', default='ERROR', required=False)
+
+    parser.add_argument('--endpoint_url', default=None, required=False, )
+
+    parser.add_argument('--hashname',
+        default='md5',
+        choices=hashlib.algorithms_available,)
+
+    if argv is None:
+        argv = parser.parse_args()
+
+    # set debugging level
+    numeric_level = getattr(logging, argv.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % argv.loglevel)
+    logging.basicConfig(level=numeric_level, )
+
+    if os.path.isdir(argv.store):
+        return check_path(argv.store)
+    elif os.path.isfile(argv.store):
+        return check_one(argv.store)
+    elif argv.store.startswith('s3:/'):
+        import boto3
+        from boto3 import client
+        from urllib.parse import urlparse
+        parts = urlparse(argv.store)
+        conn = client('s3', endpoint_url=argv.endpoint_url)
+        s3 = boto3.resource('s3', endpoint_url=argv.endpoint_url)
+        paginator = conn.get_paginator('list_objects_v2')
+        response_iterator = paginator.paginate(
+            Bucket=parts.netloc,
+            Prefix=parts.path.strip('/'),
+        )
+        for page in response_iterator:
+            for key in page['Contents']:
+                print(''.join(['s3://', parts.netloc, key['Key']]))
+                obj = s3.Object(parts.netloc, key['Key'])
+                afile = obj.get()['Body']
+                print(analyze_file(afile, 'md5'))
+    else:
+        parser.print_help()
+        return 1
+
+
+def analyze_file(afile, hashtype):
+    """Block through the file"""
+    hasher = hashlib.new(hashtype)
+    BLOCKSIZE = 1024 * hasher.block_size
+    buf = afile.read(BLOCKSIZE)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+
+def check_one(filename):
+    """Check a file hash against the filename"""
+    checksum = os.path.basename(filename)
+    with open(filename, 'rb') as afile:
+        seen_checksum = analyze_file(afile, 'md5')
+    if seen_checksum != checksum:
+        raise CheckCheckException('file {} has {} of {}'.format(
+            checksum, 'md5', seen_checksum))
+
+
+def try_one(filename):
+    try:
+        check_one(filename)
+    except CheckCheckException as e:
+        return e
+
+
+def check_path(path):
+    """Check a binary store"""
+    exit_code = 0
+    for root, ____, files, in os.walk(path):
+        for name in files:
+            error = try_one(os.path.join(root, name))
+            if error:
+                logger.error(error) 
+                exit_code = 1
+    return exit_code 
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
+"""
+Copyright © 2016, Regents of the University of California
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+- Neither the name of the University of California nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
